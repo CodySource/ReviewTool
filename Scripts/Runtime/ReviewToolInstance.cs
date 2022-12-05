14cbb50730049fd407e0dcf3e181e098e40722da
Chris Walka <walka.christopher@gmail.com>
2022-12-04 18:14:13 -0600

    Update ReviewToolInstance.cs

--- Scripts/Runtime/ReviewToolInstance.cs
+++ Scripts/Runtime/ReviewToolInstance.cs
@@ -74,7 +74,7 @@ namespace CodySource
                 if (!File.Exists($"./Assets/ReviewTool/{name}.php")) return;
 
                 string _output = File.ReadAllText($"./Assets/ReviewTool/{name}.php");
-                _output = Regex.Replace(_output, "const tableName = '.+';", $"const tableName = '{Application.productName.Replace(" ", "_")}_{Application.version.Replace(".", "_").Replace("[", "").Replace("]", "").Split('-')[0]}_Review';");
+                _output = Regex.Replace(_output, "$tableName = '.+';", $"$tableName = '{Application.productName.Replace(" ", "_")}_{Application.version.Replace(".", "_").Replace("[", "").Replace("]", "").Split('-')[0]}_Review';");
 
                 //  Write file
                 Directory.CreateDirectory("./Assets/ReviewTool/");
